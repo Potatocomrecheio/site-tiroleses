a304dbf5f041ccc31dc40b5e827081f8629f4d42
Potatocomrecheio <potatocomrecheio@gmail.com>
2025-07-02 11:19:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const startBtn = document.getElementById("startBtn");
 const backBtn = document.getElementById("backBtn");
 const welcome = document.querySelector(".welcome");
 const mainContent = document.querySelector(".main-content");
+
 const aboutBtn = document.getElementById("aboutBtn");
 const modalOverlay = document.getElementById("modalOverlay");
+
 const eventBtn = document.getElementById("eventBtn");
 const eventModal = document.getElementById("eventModal");
+const openAboutFromEvent = document.getElementById("openAboutFromEvent");
+const eventAboutModal = document.getElementById("eventAboutModal");
+
 const locationBtn = document.getElementById("locationBtn");
 const locationModal = document.getElementById("locationModal");
-const openAboutFromEvent = document.getElementById("openAboutFromEvent");
 
 startBtn.addEventListener("click", () => {
   welcome.classList.add("hidden");
@@ -16,8 +20,8 @@ startBtn.addEventListener("click", () => {
 });
 
 backBtn.addEventListener("click", () => {
-  mainContent.classList.add("hidden");
   welcome.classList.remove("hidden");
+  mainContent.classList.add("hidden");
 });
 
 aboutBtn.addEventListener("click", () => {
@@ -26,6 +30,11 @@ aboutBtn.addEventListener("click", () => {
 
 eventBtn.addEventListener("click", () => {
   eventModal.classList.add("active");
+});
+
+openAboutFromEvent.addEventListener("click", () => {
+  eventModal.classList.remove("active");
+  eventAboutModal.classList.add("active");
 });
 
 locationBtn.addEventListener("click", () => {
@@ -40,23 +49,22 @@ function closeEventModal() {
   eventModal.classList.remove("active");
 }
 
+function closeEventAboutModal() {
+  eventAboutModal.classList.remove("active");
+}
+
 function closeLocationModal() {
   locationModal.classList.remove("active");
 }
 
-openAboutFromEvent.addEventListener("click", () => {
-  eventModal.classList.remove("active");
-  modalOverlay.classList.add("active");
-});
-
-// Cores cíclicas do botão eventos
+// Efeito de cor cíclica do botão Eventos
 const eventColors = ["#e74c3c", "#2ecc71", "#3498db", "#f1c40f"];
 let colorIndex = 0;
 
 eventBtn.addEventListener("mouseover", () => {
   let interval = setInterval(() => {
     eventBtn.style.backgroundColor = eventColors[colorIndex];
-    eventBtn.style.color = "white"; // Garante que o texto continue branco
+    eventBtn.style.color = "white";
     colorIndex = (colorIndex + 1) % eventColors.length;
   }, 300);
 
